ee87167cd1637a6c4a4edbb5cdc276f268701819
Vlad-Vershinin <vladversinin676@gmail.com>
2025-10-08 15:14:40 +0500

    Setting up the server port and cors

--- server/server.API/Program.cs
+++ server/server.API/Program.cs
@@ -1,15 +1,31 @@
-namespace server.API
+namespace server.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddCors(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            options.AddPolicy("Dev", policy =>
+            {
+                policy.WithOrigins("http://localhost:3000")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            });
+        });
 
-            app.MapGet("/", () => "Hello World!");
+        var app = builder.Build();
 
-            app.Run();
-        }
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseCors("Dev");
+
+        app.UseAuthorization();
+
+        app.Run();
     }
 }
