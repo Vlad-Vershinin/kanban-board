bb9f460a3c87a44f66269f819e3baffc2104c1b8
Vlad-Vershinin <vladversinin676@gmail.com>
2025-10-09 23:16:14 +0500

    add db context and connection string

--- server/server.API/Program.cs
+++ server/server.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using server.Persistence;
+
 namespace server.API;
 
 public class Program
@@ -5,6 +8,9 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
         builder.Services.AddControllers();
         builder.Services.AddCors(options =>
@@ -20,11 +26,14 @@ public class Program
 
         var app = builder.Build();
 
+        app.UseHttpLogging();
         app.UseHttpsRedirection();
         app.UseRouting();
         app.UseCors("Dev");
 
         app.UseAuthorization();
+
+        app.MapControllers();
 
         app.Run();
     }
