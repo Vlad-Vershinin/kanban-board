d8a6ee574ba9457c1dd3a76044d72552e5b1e888
Vlad-Vershinin <vladversinin676@gmail.com>
2025-10-10 23:24:09 +0500

    fix db migrations

--- server/server.Persistence/ApplicationDbContext.cs
+++ server/server.Persistence/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using server.Domain.Models;
+using server.Persistence.Configurations;
 
 namespace server.Persistence;
 
@@ -6,8 +8,20 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+    public DbSet<User> Users { get; set; }
+    public DbSet<Board> Boards { get; set; }
+    public DbSet<UserBoardRole> UserBoardRoles { get; set; }
+    public DbSet<Column> Columns { get; set; }
+    public DbSet<Issue> Issues { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new BoardConfiguration());
+        modelBuilder.ApplyConfiguration(new UserBoardRoleConfiguration());
+        modelBuilder.ApplyConfiguration(new ColumnConfiguration());
+        modelBuilder.ApplyConfiguration(new IssueConfiguration());
+
         base.OnModelCreating(modelBuilder);
     }
 }
