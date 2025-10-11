36e3bf4b22e5c28d37805f1afac9c5b6e12c4e5d
Vlad-Vershinin <vladversinin676@gmail.com>
2025-10-11 16:20:09 +0500

    add repository interfaces and micro update on frontend

---
+++ server/server.Application/Interfaces/Repositories/IBoardRepository.cs
@@ -0,0 +1,13 @@
+﻿using server.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace server.Application.Interfaces.Repositories;
+
+public interface IBoardRepository
+{
+    Task<Board> GetBoardById(Guid id);
+    Task<List<Board>> GetBoardsByUserId(Guid id);
+    Task AddBoard(Board board);
+}
---
+++ server/server.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using server.Domain.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace server.Application.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    Task<bool> IsUserExist(User user);
+    Task<User> GetUserById(Guid id);
+    Task<User> GetUserByLogin(string login);
+    Task AddUser(User user);
+}
